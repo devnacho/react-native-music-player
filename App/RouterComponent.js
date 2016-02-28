84d4461ffb2aec2cdb61e396057e1d8bf64a7f7b
jgnatch <nachojgutierrez@gmail.com>
2016-02-28 11:24:33 +0100

    initial commit

---
+++ App/RouterComponent.js
@@ -0,0 +1,46 @@
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class RouterComponent extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Artists List
+        </Text>
+        <Text style={styles.instructions}>
+          The artist list goes here
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#111',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#fff',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#888',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('RouterComponent', () => RouterComponent);
+
